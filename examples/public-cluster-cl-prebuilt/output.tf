02a44500ad6ef1d7b708cb8156c64c7ae3caa910
Yann DEGAT <yann.degat@gmail.com>
2018-03-02 19:41:11 +0100

    Add coreos & centos 7 prebuilt examples

---
+++ examples/public-cluster-cl-prebuilt/output.tf
@@ -0,0 +1,11 @@
+locals {
+  test_command = "/opt/etcd/bin/etcdctl --ca-file /opt/etcd/certs/ca.pem --cert-file /opt/etcd/certs/cert.pem --key-file /opt/etcd/certs/cert-key.pem --endpoints https://${module.etcd.public_ipv4_addrs[0]}:2379 member list"
+}
+
+output "tf_test" {
+  value = <<TEST
+ssh -o UserKnownHostsFile=/dev/null \
+    -o StrictHostKeyChecking=no \
+    core@${module.etcd.public_ipv4_addrs[0]} sh -c '"[ \$(${local.test_command} | wc -l) == ${var.count} ] && ${local.test_command} | grep -q isLeader=true"'
+TEST
+}
