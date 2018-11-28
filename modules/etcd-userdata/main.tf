93f9527e6a4980bd7fe789ace3a2baafc357b639
Yann DEGAT <yann.degat@gmail.com>
2018-11-28 10:19:14 +0100

    cfssl: bump to v0.1.12

--- modules/etcd-userdata/main.tf
+++ modules/etcd-userdata/main.tf
@@ -4,7 +4,7 @@ locals {
 
 module "cfssl" {
   source  = "ovh/publiccloud-cfssl/ovh//modules/cfssl-userdata"
-  version = ">= 0.1.11"
+  version = ">= 0.1.12"
 
   ignition_mode        = "${var.ignition_mode}"
   cidr                 = "${var.cidr}"
