df20488aa789bb3308942002f66b340e83ef265b
Yann DEGAT <yann.degat@gmail.com>
2019-04-04 14:30:40 +0200

    postinstall: fix call to shellscript

--- modules/install-etcd/main.tf
+++ modules/install-etcd/main.tf
@@ -6,10 +6,10 @@ resource "null_resource" "post_install_etcd" {
   }
 
   connection {
-    host                = "${element(var.ipv4_addrs, count.index)}"
-    user                = "${var.ssh_user}"
-    bastion_host        = "${var.ssh_bastion_host}"
-    bastion_user        = "${var.ssh_bastion_user}"
+    host         = "${element(var.ipv4_addrs, count.index)}"
+    user         = "${var.ssh_user}"
+    bastion_host = "${var.ssh_bastion_host}"
+    bastion_user = "${var.ssh_bastion_user}"
   }
 
   provisioner "remote-exec" {
@@ -24,8 +24,8 @@ resource "null_resource" "post_install_etcd" {
   provisioner "remote-exec" {
     inline = [
       "/bin/bash /tmp/install-etcd/system-upgrade.sh",
-      "/bin/sh /tmp/install-etcd/install-etcd --path ${var.install_dir} --version ${var.etcd_version} --sha256sum ${var.etcd_sha256sum}",
-      "echo start etcd; sudo systemctl restart etcd.service || true"
+      "/bin/bash /tmp/install-etcd/install-etcd --path ${var.install_dir} --version ${var.etcd_version} --sha256sum ${var.etcd_sha256sum}",
+      "echo start etcd; sudo systemctl restart etcd.service || true",
     ]
   }
 }
